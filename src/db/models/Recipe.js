f2ce1a2b5a854a8a043fd2a2fdc74d2ed09292d9
Halyna Moldovianu <Beorgeos@gmail.com>
2025-07-18 14:14:32 +0200

    recipe model added

---
+++ src/db/models/Recipe.js
@@ -0,0 +1,53 @@
+import { model, Schema } from 'mongoose';
+import { saveErrorHandler, setUpdateSettings } from './hooks.js';
+
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      maxlength: 64,
+      required: true,
+    },
+
+    description: { type: String, maxlength: 200, required: true },
+
+    time: { type: Number, required: true, min: 1, max: 360 },
+
+    calories: {
+      type: Number,
+      min: 1,
+      max: 10000,
+    },
+    category: {
+      type: String,
+      ref: 'categories',
+      required: true,
+    },
+    ingredients: {
+      type: String,
+      ref: 'ingredients',
+      required: true,
+    },
+    instructions: {
+      type: String,
+      required: true,
+      maxlength: 1200,
+    },
+    thumb: {
+      type: String,
+      maxlength: 255,
+    },
+    owner: {
+      type: String,
+      ref: 'users',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+recipeSchema.post('save', saveErrorHandler);
+recipeSchema.pre('findOneAndUpdate', setUpdateSettings);
+
+export const Recipe = model('Recipe', recipeSchema);

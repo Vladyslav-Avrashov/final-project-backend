69f62724dc56e2eb2fe6307e286070d32a554434
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-07-18 15:41:05 +0300

    work with swagger Pereverza

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,7 +3,6 @@ import { authenticate } from '../middlewares/authenticate.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { currentUserController } from '../controllers/auth.js';
 
-
 const usersRouter = Router();
 
 usersRouter.get('/current', authenticate, ctrlWrapper(currentUserController));

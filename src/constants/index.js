70940e592f71f9455b6d0220f017581fa7ff18b7
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2025-07-18 11:52:26 -0500

    fix validation of email, name and password in login and register Joi validation schema, added validation rules to documentation

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
-export const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+export const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export const getAccessTokenValidUntil = () =>
   new Date(Date.now() + 15 * 60 * 1000);

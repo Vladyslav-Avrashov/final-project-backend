ec0188c96c9d86766eaf455af9fac7d097776989
IrynaYakymenko <irynayakymenko93@gmail.com>
2025-07-21 23:04:23 +0300

    createGetIngredients

--- src/routers/ingredients.js
+++ src/routers/ingredients.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { getIngredients } from '../controllers/getIngredients.js';
 
 const ingredientsRouter = Router();
+
+ingredientsRouter.get('/', getIngredients);
 
 export default ingredientsRouter;
